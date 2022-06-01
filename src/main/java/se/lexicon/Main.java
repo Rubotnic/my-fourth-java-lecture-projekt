36ea817cc33bd72e88ccf8b7db040dcff88ed858
Rubotnic <info@rubotnic.com>
2022-06-01 14:36:26 +0200

    My fourth JAVA lecture projekt

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,11 @@
+package se.lexicon;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int a = 23;
+        int b = 11;
+        int c = 77;
+        System.out.print("(23 + 11 + 77) / 3 = " + (a + b + c) / 3);
+    }
+}
